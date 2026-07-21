0540c5df7c3313e9f6934da101c1d1cc7ae9ac72
Hasin <hasin@example.com>
2026-07-21 10:38:59 +0530

    first commit

---
+++ Week2/functions/fun3.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+function checkEligibility(name: string, age: number): string {
+    if (age >= 18) {
+        return `${name} is eligible to vote.`;
+    } else {
+        return `${name} is not eligible to vote.`;
+    }
+}
+
+console.log(checkEligibility("Rahul", 20));

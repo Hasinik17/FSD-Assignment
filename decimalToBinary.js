a377d33d40016b00eab617d95281e083d7da3eee
HASINI KOKKERAGADDA <24b01a4561@svecw.edu.in>
2026-08-08 14:24:53 +0530

    Delete decimalToBinary.js

--- decimalToBinary.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//======================================================
-// Program: Convert Binary to Decimal
-//======================================================
-Object.defineProperty(exports, "__esModule", { value: true });
-let binary = "11001";
-let decimal = parseInt(binary, 2);
-console.log("Decimal =", decimal);

4594519c3b7b5380b27b07ba7e05fcf8f74fcc39
HASINI KOKKERAGADDA <24b01a4561@svecw.edu.in>
2026-08-08 14:09:52 +0530

    Delete perfectNumber.js

--- perfectNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//======================================================
-// Program: Convert Decimal to Binary
-//======================================================
-Object.defineProperty(exports, "__esModule", { value: true });
-let decimal = 25;
-console.log("Binary =", decimal.toString(2));
